5a4e9d81d0658c0296ae83d69a47308786fa27b6
hrushi20 <hrushikesh20thegreat@gmail.com>
2023-01-15 02:47:39 +0530

    Added autobuild

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
-
-const app = express();
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
 
 const PORT = 8082;
+const app = express();
 
+
+app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.post("/api/triggerBuild",(req,res,next)=>{
+	console.log(req.body);
+	console.log("Inside post req");
+	return res.json({name:"Thank you"});
+});
 
 app.listen(PORT, ()=>{
 	console.log("Server running on ",PORT);
